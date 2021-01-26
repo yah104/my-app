66136fba304aa545ce29ef200b014088e79e57ca
Yanling <yah104@ucsd.edu>
2021-01-25 16:42:57 -0800

    copy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import profileImage from './img/dai3.jpg';
+import React, { Component } from 'react';
 import './App.css';
+import Sidebar from './components/sidebar'
+import Introduction from './components/introduction'
+import About from './components/about'
+import Timeline from './components/timeline'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My React App Dog!</h1>
-      <h2>Yanling Huang</h2>
-      <img src={profileImage} alt="profile-image" />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div id="colorlib-page">
+        <div id="container-wrap">
+         	<Sidebar></Sidebar>
+				<div id="colorlib-main">
+					<Introduction></Introduction>
+					<About></About>
+					<Timeline></Timeline>
+          	</div>
+      	</div>
+      </div>
+    );
+  }
 }
 
 export default App;
