c457c29fb8790048878b4c67a80ce56c56b5a3b3
Yanling <yah104@ucsd.edu>
2021-01-25 20:32:53 -0800

    color

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -14,7 +14,7 @@ export default class Introduction extends Component {
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
-                          <h1>Hi! <br />I'm Yanling Huang</h1>
+                          <h1 style={{color:"orange"}}>Hi! <br />I'm Yanling Huang</h1>
                           <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1tMqwB8g-1cZ6-BJc6FqCa5yRPTiJEcZt/view?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
                         </div>
                       </div>
@@ -29,7 +29,7 @@ export default class Introduction extends Component {
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>I love learning<br />and building!!</h1>
+                          <h1 style={{color:"orange"}}>I love learning<br />and building!!</h1>
                           <p><a className="btn btn-primary btn-learn" href="https://github.com/yah104" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
                         </div>
                       </div>
